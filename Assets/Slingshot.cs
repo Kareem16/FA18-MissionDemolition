1604bc243f8b857e2e1712de490dc018e37001c8
Kareem16 <35926953+Kareem16@users.noreply.github.com>
2018-10-05 18:52:15 -0400

    Done In Class 10/5 (1)
    Continue @ Showing When the Slingshot Is Active page 619

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+    private void OnMouseEnter() {
+        print("Slingshot:OnMouseEnter()");
+    }
+
+    private void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+    }
+}
